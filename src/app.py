7960e3083ae928523d3d41d2e8175c805a55cfb1
Danilo Miranda <danilo030920@gmail.com>
2025-12-03 14:26:25 -0300

    Adds python-app application with k8s deployments

---
+++ src/app.py
@@ -0,0 +1,19 @@
+import socket
+from datetime import datetime
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/api/v1/details')
+def details():
+  return jsonify({
+    'time': datetime.now().strftime("%I:%M:%S%p on %B %d, %Y"),
+    'hostname': socket.gethostname()
+  }), 200
+
+@app.route('/api/v1/health')
+def health():
+  return jsonify({ 'status': 'up' }), 200
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0')
