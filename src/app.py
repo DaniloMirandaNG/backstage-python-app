94c067f493f757cd05ae1bf9d867619a2ce3e0c0
Danilo Miranda <danilo030920@gmail.com>
2025-12-04 17:15:30 -0300

    Adds new info endpoint

--- src/app.py
+++ src/app.py
@@ -4,12 +4,13 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/api/v1/details')
-def details():
+@app.route('/api/v1/info')
+def info():
   return jsonify({
     'time': datetime.now().strftime("%I:%M:%S%p on %B %d, %Y"),
     'hostname': socket.gethostname(),
-    'message': 'You are doing great, human!! :)'
+    'message': 'You are doing great, human!! :)',
+    'deployed_on': 'kubernetes'
   }), 200
 
 @app.route('/api/v1/health')
